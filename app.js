f9df695bde001b0a2e3d037ec1a0343a0e02d5db
JonaMend <jonamendoza.olv@gmail.com>
2023-07-06 11:42:50 -0500

    Realizado

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// Escribe tu código aquí.
+//Jonathan Emanuel Mendoza Olvera
+
+function bigWords(string, array) {
+    const result = [];
+    for (let i = 0; i< array.length; i++)
+    if(array[i].length>string.length){
+        result.push(array[i]);
+    }
+    return result;
+}
+
+const Amistad = ['Karla', 'hugo', 'paco', 'carmen', 'blanca', 'jonathan'];
+const result = bigWords('pedro', Amistad);
+console.log(result);
+
+function printArray(array) {
+    const ul = document.createElement('ul');
+    for (let i = 0; i < array.length; i++) {
+        const li = document.createElement('li')
+        li.textContent=array[i];
+        ul.appendChild(li);
+
+    }
+
+    document.body.appendChild(ul);
+
+}
+
+const myArray2 =['Karla', 'paco', 'blanca']
+printArray(myArray2);
